5dedcc77093d86b3e1ccbe25323bda813724ee84
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-08-09 01:01:18 +0900

    test: mint one sbt token to the another

--- test/test.js
+++ test/test.js
@@ -1,14 +1,28 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+let owner;
+let secondAddr;
+let ReputationTokenFactory;
+let ReputationToken;
 
 describe("Reputation Token", function () {
-  it("The initial Deployment with totalSupply is zero", async function () {
-    const [owner] = await ethers.getSigners();
-
-    const ReputationTokenFactory = await ethers.getContractFactory("ReputationToken");
-
-    const ReputationToken = await ReputationTokenFactory.deploy();
+  it("should execute initial Deployment with totalSupply is zero", async function () {
+    [owner] = await ethers.getSigners();
+    ReputationTokenFactory = await ethers.getContractFactory("ReputationToken");
+    ReputationToken = await ReputationTokenFactory.deploy();
 
     const ownerBalance = await ReputationToken.balanceOf(owner.address);
     expect(await ReputationToken.totalSupply()).to.equal(ownerBalance);
   });
+
+  it("should mint one SBT token to the another address", async() => {
+    [owner, secondAddr] = await ethers.getSigners();
+    const URI = "Reputation Token";
+    const SIGNATURE = ethers.utils.formatBytes32String("SIGNATURE");
+    const receipt = await ReputationToken.give(owner.address, secondAddr.address, URI, SIGNATURE);
+    console.log(receipt);
+    
+    expect(await ReputationToken.balanceOf(secondAddr.address)).to.equal(1);
+  })
 });
